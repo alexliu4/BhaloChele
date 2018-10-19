d5ae1f719553dbb158aa116d5a1165a9ceed12c2
ShinBamba <sbamba@stuy.edu>
2018-10-19 14:03:44 -0400

    incoporated login logout

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
 import os
 
-from flask import Flask, render_template, redirect, session, flash
+from flask import Flask, render_template, redirect, session, flash, request, url_for
 app = Flask(__name__)
 
 app.secret_key = os.urandom(32) #generate random key to use session for cookies
 
-user = 'shin'
-passwd = 'bangla'
+users = {'sh':'hi'}
 
 @app.route("/")
 def home():
-    if 'shin' in session: #if a user is logged in
-        return render_template("homepage.html", user = 'shin')#send to welcome page
+    if 'sh' in session: #if a user is logged in
+        return render_template("homepage.html", user = 'sh')#send to welcome page
     else:
-        return render_template("login.html")#send to login page
+        return render_template("login.html")#login page
 
 @app.route("/auth", methods = ['POST'])
 def login():
-    if request.form['username'] == user and request.form['password'] == passwd:
-        session[request.form['username']] = request.form['password'] #logs in user
+    if request.form['username'] in users and request.form['password'] == users[request.form['username']]:
+        session['sh'] = 'hi' #logs in user
         return redirect(url_for('home'))#send to welcome page
     else:
         flash("invalid username/password. Please try again. If you do not have an account please register")
         return redirect(url_for("login"))
 
+@app.route("/logout", methods = ["POST", "GET"])
+def gohome():
+	session.pop('sh',None)#logs out user. None used if no users are logged in
+	return redirect(url_for('home'))#Send to login page
+
+
+    
 @app.route("/register")
 def register():
-    
+    return 'lol'
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
