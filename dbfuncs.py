11ce4d95f6624299d8c4ffdc6a4284acb35405e8
ShinBamba <sbamba@stuy.edu>
2018-10-23 14:03:05 -0400

    WOWW SO BROKE MAN YES HEEHAW

--- dbfuncs.py
+++ dbfuncs.py
@@ -27,17 +27,31 @@ def add_to_viewed_stories(acc_id, title):
     c.execute("INSERT INTO {0} VALUES( {1}, '{2}');".format('stories_viewable', acc_id, title))
 
 def add_text(acc_id, title, text):
-    add_to_viewed_stories(acc_id, title)
-    c.execute("SELECT entry_id FROM {0}".format(title))
-    entry_id = 0
-    for thing in c:
-        id = thing[0]
-    c.execute("INSERT INTO {0} VALUES( {1}, '{2}');".format(title, entry_id+1, text))
+	DB_FILE="curbur.db"
+
+	db = sqlite3.connect(DB_FILE,check_same_thread=False) #open if file exists, otherwise create
+	c = db.cursor()               #facilitate db ops
+
+	add_to_viewed_stories(acc_id, title)
+	c.execute("SELECT entry_id FROM {0}".format(title))
+	entry_id = 0
+	for thing in c:
+		id = thing[0]
+	c.execute("INSERT INTO {0} VALUES( {1}, '{2}');".format(title, entry_id+1, text))
+	db.commit() #save changes
+	db.close() #close database
 
 def add_new_story(acc_id,title,text):
+	DB_FILE="curbur.db"
+
+	db = sqlite3.connect(DB_FILE,check_same_thread=False) #open if file exists, otherwise create
+	c = db.cursor()               #facilitate db ops
+
     add_to_viewed_stories(acc_id, title)
     c.execute("CREATE TABLE {0} ({1} INTEGER PRIMARY KEY, {2} TEXT UNIQUE);".format(title, "entry_id", "entry"))
     c.execute("INSERT INTO {0} VALUES( {1}, '{2}');".format(title, 0, text))
+	db.commit() #save changes
+	db.close() #close database
 
 def get_accounts(user):
 	DB_FILE="curbur.db"
